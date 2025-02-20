c3a4416300aebcdf3013526ef93ba1d3b07eadac
Yotam Aviv <yotam@aviv.net>
2025-02-19 20:09:07 -0500

    clouddistinct in emulator

--- functions/index.js
+++ functions/index.js
@@ -194,3 +194,101 @@ exports.signin = onRequest((req, res) => {
       res.status(201).json({});
     });
 });
+
+function keyToken(dict) {
+  const sortedKeys = Object.keys(dict).sort();
+  let resultString = "";
+  for (const key of sortedKeys) {
+    resultString += dict[key];
+  }
+  return resultString;
+}
+
+const contentVerbs = [
+  'rate',
+  'clear',
+  'follow',
+  'censor',
+  'relate',
+  'dontRelate',
+  'equate',
+  'dontEquate',
+];
+
+function getVerbSubject(j) {
+  for (var verb of contentVerbs) {
+    if (j[verb] != null) {
+      return [verb, j[verb]];
+    }
+  }
+  return null;
+}
+
+function clouddistinct(input) {
+  var out = [];
+  var already = new Set();
+  for (var j of input) {
+    var i = j['I'];
+    const [verb, subject] = getVerbSubject(j);
+    var key = keyToken(subject);
+    if (already.has(key)) continue;
+    already.add(key);
+    if (verb == 'clear') continue;
+    delete j.I;
+    delete j.statement;
+    delete j.signature;
+    delete j.previous;
+    out.push(j);
+  }
+  return out;
+}
+
+/// Works on emulator: http://127.0.0.1:5001/nerdster/us-central1/clouddistinct?token=f4e45451dd663b6c9caf90276e366f57e573841b
+exports.clouddistinct = onRequest(async (req, res) => {
+  const token = req.query.token;
+  if (!token) return res.status(400).send('Missing token');
+
+  // I'm not commenting out the Oneofus verbs because I often run the emulator from the nerdster 
+  // directory. Sloppy, not correct..
+  const key2order = {
+    'statement': 0, 'time': 1, 'I': 2,
+    'clear': 7,
+    // Oneofus verbs
+    'trust': 3, 'block': 4, 'replace': 5, 'delegate': 6,
+    // Nerdster verbs
+    'rate': 8, 'censor': 9, 'relate': 10, 'dontRelate': 11, 'equate': 12, 'dontEquate': 13, 'follow': 14,
+    'with': 16,
+    // Oneofus with
+    'moniker': 18, 'revokeAt': 19, 'domain': 20,
+    // Nerdster with
+    'tags': 21, 'recommend': 22, 'dismiss': 23, 'stars': 24, 'comment': 25, 'contentType': 26, 'other': 17,
+    'previous': 27, 'signature': 28
+  };
+
+  try {
+    const db = admin.firestore();
+    const collectionRef = db.collection(token).doc('statements').collection('statements');
+    const snapshot = await collectionRef.orderBy('time', 'desc').get();
+    const data = snapshot.docs.map(doc => doc.data());
+
+    var data2 = [];
+    for (const datum of data) {
+      const orderedDatum = Object.keys(datum)
+        .sort((a, b) => ((key2order[a] ?? 40) - (key2order[b] ?? 40)))
+        // .sort((a, b) => compareKeys(a, b))
+        .reduce((obj, key) => {
+          obj[key] = datum[key];
+          return obj;
+        }, {});
+      data2.push(orderedDatum);
+    }
+
+    var data3 = clouddistinct(data2);
+
+
+    res.status(200).json(data3);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error exporting collection');
+  }
+});
