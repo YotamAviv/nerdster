fcaf05825566de0a0804a48236b8d86dfce5675e
Yotam Aviv <yotam@aviv.net>
2025-02-26 08:41:01 -0500

    missing file, oops

---
+++ lib/dev/corruption_check.dart
@@ -0,0 +1,41 @@
+import 'package:nerdster/comp.dart';
+import 'package:nerdster/oneofus/fetcher.dart';
+import 'package:nerdster/oneofus/statement.dart';
+import 'package:nerdster/oneofus/trust_statement.dart';
+import 'package:nerdster/oneofus/util.dart';
+import 'package:nerdster/singletons.dart';
+
+class CorruptionCheck {
+  static Future<void> make() async {
+    await Comp.waitOnComps([oneofusNet, keyLabels]);
+
+    for (String oneofus in oneofusNet.network.keys) {
+      Fetcher fetcher = Fetcher(oneofus, kOneofusDomain);
+      String? previousToken;
+      DateTime? previousTime;
+      for (Statement s in await fetcher.fetchAllNoVerify()) {
+        String token = s.token;
+        DateTime time = parseIso(s.json['time']);
+
+        // Validate notary chain, decending order
+        if (previousTime == null) {
+          // no check
+        } else {
+          if (s.token != previousToken) {
+            String error = '${keyLabels.labelKey(oneofus)} $time !notary: $token != $previousToken';
+            print(error);
+          }
+
+          if (!time.isBefore(previousTime)) {
+            String error = '${keyLabels.labelKey(oneofus)} $time !desc: $time >= $previousTime';
+            print(error);
+          }
+        }
+
+        previousToken = s.json['previous'];
+        previousTime = time;
+      }
+    }
+    print('out');
+  }
+}
