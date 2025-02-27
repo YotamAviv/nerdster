4fdde0babae0f3aabdf3637fd5a8a4566e28fad6
Yotam Aviv <yotam@aviv.net>
2025-02-27 15:24:25 -0500

    error when can't launch URL

--- lib/util_ui.dart
+++ lib/util_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nerdster/oneofus/ui/alert.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 const Color linkColor = Colors.blueAccent;
@@ -7,22 +8,15 @@ final Color linkColorDisabled = Colors.blue.shade200;
 const TextStyle hintStyle = TextStyle(color: Colors.black26);
 const TextStyle linkStyle = TextStyle(color: linkColor, decoration: TextDecoration.underline);
 
-// TODO: Add param context, show alert if can't
-Future<void> myLaunchUrl(String url) async {
+Future<void> myLaunchUrl(String url, BuildContext context) async {
   final Uri uri = Uri.parse(url);
-  /* BAD
-  if (!await launchUrl(url)) {
-    print('Exception');
-    // Happens every time. Ignore: throw Exception('Could not launch $_url');
-  }
-  */
   if (await canLaunchUrl(uri)) {
     try {
       await launchUrl(uri);
-    } catch(e) {
-      print(e);
+    } catch (e) {
+      await alertException(context, e);
     }
   } else {
-    print ('Could not launch $uri');
+    await alert('Bad URL?', 'Could not launch "$uri"', ['okay'], context);
   }
 }
