513a5950f7a73ed22e34543703102f23cc79e44e
Yotam Aviv <yotam@aviv.net>
2025-03-01 21:25:27 -0500

    WIP

--- lib/comp.dart
+++ lib/comp.dart
@@ -102,6 +102,7 @@ abstract mixin class Comp {
     }
     // calling process has been initiated; just wait..
     await ValueWaiter(_ready, true).untilReady();
+    // NEXT: TEMP: Do we want to throw the same exception at something else? Probably not.
     // if (b(_exception)) {
     //   // print('Throwing: $_exception');
     //   // QUESTIONABLE: _ready.value = false; // See docs at top about semantics of 'ready'.
