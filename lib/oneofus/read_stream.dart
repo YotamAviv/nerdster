2b8ce5d1d8780ff3617f771661a2c652e02f66c6
Yotam Aviv <yotam@aviv.net>
2025-04-04 19:49:24 -0400

    pushing functions to prod to check CORS and whatnot..

--- lib/oneofus/read_stream.dart
+++ lib/oneofus/read_stream.dart
@@ -1,13 +1,39 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:nerdster/main.dart';
 import 'package:nerdster/oneofus/jsonish.dart';
+import 'package:nerdster/oneofus/trust_statement.dart';
+import 'package:nerdster/content/content_statement.dart';
+
+Map<FireChoice, Map<String, String>> streamstatementsUrl = {
+  FireChoice.prod: {
+    kOneofusDomain: 'http://us-central1-one-of-us-net.cloudfunctions.net/streamstatements',
+    kNerdsterDomain: 'http://us-central1-one-of-us-net.cloudfunctions.net/streamstatements'
+  },
+  FireChoice.emulator: {
+    kOneofusDomain: 'http://127.0.0.1:5001/one-of-us-net/us-central1/streamstatements',
+    kNerdsterDomain: 'http://127.0.0.1:5001/nerdster/us-central1/streamstatements'
+  },
+};
+
+
+Map<FireChoice, Map<String, String>> streamnumsUrl = {
+  FireChoice.prod: {
+    kOneofusDomain: 'http://us-central1-one-of-us-net.cloudfunctions.net/streamnums',
+    kNerdsterDomain: 'http://us-central1-one-of-us-net.cloudfunctions.net/streamnums'
+  },
+  FireChoice.emulator: {
+    kOneofusDomain: 'http://127.0.0.1:5001/one-of-us-net/us-central1/streamnums',
+    kNerdsterDomain: 'http://127.0.0.1:5001/nerdster/us-central1/streamnums'
+  },
+};
 
 void readStream() async {
   var client = http.Client();
   try {
-    final http.Request request =
-        http.Request('GET', Uri.parse("http://127.0.0.1:5001/nerdster/us-central1/streamnums"));
+    final Uri uri = Uri.parse(streamnumsUrl[fireChoice]![kNerdsterDomain]!);
+    final http.Request request = http.Request('GET', uri);
     final http.StreamedResponse response = await client.send(request);
     assert(response.statusCode == 200, 'Request failed with status: ${response.statusCode}');
     response.stream.listen((value) {
