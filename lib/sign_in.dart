4892b3efdbd0dac5e68e915cd182fed38e7c6dbf
Yotam Aviv <yotam@aviv.net>
2025-07-28 10:25:53 -0700

    ..

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -137,73 +137,72 @@ Future<void> signIn(OouPublicKey oneofusPublicKey, OouKeyPair? nerdsterKeyPair, 
   await BarRefresh.refresh(context);
 }
 
-Future<void> qrSignin(BuildContext context) async {
-  Map<String, dynamic> forPhone = <String, dynamic>{};
-  forPhone['domain'] = kNerdsterDomain;
-  forPhone['method'] = 'POST';
-  forPhone['uri'] = (fireChoice == FireChoice.emulator)
-      ? 'http://127.0.0.1:5001/nerdster/us-central1/signin'
-      : 'https://signin.nerdster.org/signin';
+// Future<void> qrSignin(BuildContext context) async {
+//   Map<String, dynamic> forPhone = <String, dynamic>{};
+//   forPhone['domain'] = kNerdsterDomain;
+//   forPhone['method'] = 'POST';
+//   forPhone['uri'] = (fireChoice == FireChoice.emulator)
+//       ? 'http://127.0.0.1:5001/nerdster/us-central1/signin'
+//       : 'https://signin.nerdster.org/signin';
 
-  final PkeKeyPair keyPair = await crypto.createPke();
-  final PkePublicKey publicKey = await keyPair.publicKey;
-  final Map<String, dynamic> publicKeyJson = await publicKey.json;
-  final String session = getToken(publicKeyJson);
-  forPhone['session'] = session;
-  forPhone['publicKey'] = publicKeyJson;
+//   final PkeKeyPair keyPair = await crypto.createPke();
+//   final PkePublicKey publicKey = await keyPair.publicKey;
+//   final Map<String, dynamic> publicKeyJson = await publicKey.json;
+//   final String session = getToken(publicKeyJson);
+//   forPhone['session'] = session;
+//   forPhone['publicKey'] = publicKeyJson;
 
-  final firestore = FirebaseFirestore.instance;
-  final completer = Completer<void>();
-  StreamSubscription? subscription;
+//   final firestore = FirebaseFirestore.instance;
+//   final completer = Completer<void>();
+//   StreamSubscription? subscription;
 
-  ValueNotifier<bool> storeKeys = ValueNotifier<bool>(true);
+//   ValueNotifier<bool> storeKeys = ValueNotifier<bool>(true);
 
-  // Start listening BEFORE showing dialog
-  subscription = firestore
-      .collection('sessions')
-      .doc('doc') // FIXME: is 'doc' correct here? This seems placeholder-ish.
-      .collection(session)
-      .snapshots()
-      .listen((QuerySnapshot<Map<String, dynamic>> docSnapshots) async {
-    if (docSnapshots.docs.isEmpty || completer.isCompleted) return;
-    completer.complete(); // signal that something arrived
-    await subscription!.cancel();
+//   // Start listening BEFORE showing dialog
+//   subscription = firestore
+//       .collection('sessions')
+//       .doc('doc') // FIXME: is 'doc' correct here? This seems placeholder-ish.
+//       .collection(session)
+//       .snapshots()
+//       .listen((QuerySnapshot<Map<String, dynamic>> docSnapshots) async {
+//     if (docSnapshots.docs.isEmpty || completer.isCompleted) return;
+//     completer.complete(); // signal that something arrived
+//     await subscription!.cancel();
 
-    Map<String, dynamic>? data = docSnapshots.docs.first.data();
+//     Map<String, dynamic>? data = docSnapshots.docs.first.data();
 
-    // Unpack Oneofus public key
-    Json identityJson = data[kOneofusDomain]!;
-    OouPublicKey oneofusPublicKey = await crypto.parsePublicKey(identityJson);
+//     // Unpack Oneofus public key
+//     Json identityJson = data[kOneofusDomain]!;
+//     OouPublicKey oneofusPublicKey = await crypto.parsePublicKey(identityJson);
 
-    // Optionally unpack and decrypt Nerdster private key
-    Json? delegateJson;
-    OouKeyPair? nerdsterKeyPair;
-    if (b(data['publicKey'])) {
-      PkePublicKey phonePkePublicKey = await crypto.parsePkePublicKey(data['publicKey']);
+//     // Optionally unpack and decrypt Nerdster private key
+//     Json? delegateJson;
+//     OouKeyPair? nerdsterKeyPair;
+//     if (b(data['publicKey'])) {
+//       PkePublicKey phonePkePublicKey = await crypto.parsePkePublicKey(data['publicKey']);
 
-      String? delegateCiphertext = data['delegateCiphertext'];
-      String? delegateCleartext = data['delegateCleartext'];
-      assert(!(b(delegateCiphertext) && b(delegateCleartext)));
+//       String? delegateCiphertext = data['delegateCiphertext'];
+//       String? delegateCleartext = data['delegateCleartext'];
+//       assert(!(b(delegateCiphertext) && b(delegateCleartext)));
 
-      if (b(delegateCiphertext)) {
-        print('delegate key encrypted: YES');
-        delegateCleartext = await keyPair.decrypt(delegateCiphertext!, phonePkePublicKey);
-      } else {
-        print('delegate key encrypted: NO');
-      }
+//       if (b(delegateCiphertext)) {
+//         print('delegate key encrypted: YES');
+//         delegateCleartext = await keyPair.decrypt(delegateCiphertext!, phonePkePublicKey);
+//       } else {
+//         print('delegate key encrypted: NO');
+//       }
 
-      delegateJson = jsonDecode(delegateCleartext!);
-      nerdsterKeyPair = await crypto.parseKeyPair(delegateJson!);
-    }
+//       delegateJson = jsonDecode(delegateCleartext!);
+//       nerdsterKeyPair = await crypto.parseKeyPair(delegateJson!);
+//     }
 
-    // Dismiss dialog if still open
-    if (context.mounted) Navigator.of(context).pop();
+//     // Dismiss dialog if still open
+//     if (context.mounted) Navigator.of(context).pop();
 
-    // Proceed with sign-in
-    await signIn(oneofusPublicKey, nerdsterKeyPair, storeKeys.value, context);
-  });
+//     // Proceed with sign-in
+//     await signIn(oneofusPublicKey, nerdsterKeyPair, storeKeys.value, context);
+//   });
 
-// chatGPT's change so that I stop listening for Firebase event if user dismisses dialog
 //   // Show the dialog
 //   await showDialog(
 //     context: context,
