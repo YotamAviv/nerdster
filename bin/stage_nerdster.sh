1a9d2686724daf5a948f89514ffbd71e3135f102
Yotam Aviv <yotam@aviv.net>
2025-11-19 16:32:03 -0500

    ..

--- bin/stage_nerdster.sh
+++ bin/stage_nerdster.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 mkdir -p build/web
-# TEMP: TODO: This deletes flutter_bootsrap.js rsync -a --delete web/ build/web/
+# TODO: This deletes flutter_bootsrap.js 
+# rsync -a --delete web/ build/web/
+rsync -a web/ build/web/
 if [[ "${1:-}" == "deploy" ]]; then
   firebase deploy --only hosting --project=nerdster
 fi
