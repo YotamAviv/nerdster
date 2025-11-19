419eb2c71705015614f9a82a118028a8c99c51c9
Yotam Aviv <yotam@aviv.net>
2025-11-19 16:32:03 -0500

    ..

--- bin/stage_oneofus.sh
+++ bin/stage_oneofus.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 mkdir -p build/web
-# TEMP: TODO: rsync -a --delete web/ build/web/
+# TODO: This deletes flutter_bootsrap.js 
+# rsync -a --delete web/ build/web/
+rsync -a web/ build/web/
 cp web/oneofus.html build/web/index.html
-
 if [[ "${1:-}" == "deploy" ]]; then
   firebase deploy --only hosting --project=one-of-us-net
 fi
