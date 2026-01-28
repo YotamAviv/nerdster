9c1176b7718fcaef401ec68541ff1cb052aa4969
Yotam Aviv <yotam@aviv.net>
2026-01-28 08:00:12 -0500

    ..

--- bin/integration_test.sh
+++ bin/integration_test.sh
@@ -3,10 +3,10 @@
 # Integration test command from docs/testing.md
 flutter drive \
   --driver=test_driver/integration_test.dart \
-  --target=integration_test/v2_basic_test.dart \
+  --target=integration_test/basic_test.dart \
   -d chrome
 
 flutter drive \
   --driver=test_driver/integration_test.dart \
-  --target=integration_test/v2_ui_test.dart \
+  --target=integration_test/ui_test.dart \
   -d chrome
