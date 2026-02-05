d1a20d078e4c0573f22d4f9a3f88f038d95c2e96
Yotam Aviv <yotam@aviv.net>
2026-02-04 22:23:53 -0500

    nerdster man page

--- bin/stage_nerdster.sh
+++ bin/stage_nerdster.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 mkdir -p build/web
 rsync -a web/ build/web/
+cp web/nerdster_man.html build/web/man.html
 if [[ "${1:-}" == "deploy" ]]; then
   firebase deploy --only hosting --project=nerdster
 fi
--- bin/stage_oneofus.sh
+++ bin/stage_oneofus.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 mkdir -p build/web
 rsync -a web/ build/web/
+cp web/oneofus_man.html build/web/man.html
 cp web/oneofus.html build/web/index.html
 if [[ "${1:-}" == "deploy" ]]; then
   firebase deploy --config oneofus.firebase.json --only hosting --project=one-of-us-net
