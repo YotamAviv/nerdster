70163d71219dc85a332d4b310372fec730f1312f
Yotam Aviv <yotam@aviv.net>
2026-03-30 13:32:00 -0400

    Fix external URL launching on Android 11+ and remove dead code
    - AndroidManifest.xml: Added `<queries>` block for `https` and `http` intents.
      This satisfies Android 11+ (API 30) package visibility requirements so
      url_launcher can successfully open external browser apps. Fixes the Terms
      of Service and Safety Policy links failing silently on physical Android devices.
    - util_ui.dart: Removed unused legacy `myLaunchUrl` function and its
      associated imports to clean up dead code.
    - pubspec.yaml: Bump version to 1.1.13+178 for release.

--- lib/ui/util_ui.dart
+++ lib/ui/util_ui.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nerdster/ui/util/alert.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 const Color linkColor = Colors.blueAccent;
 final Color linkColorAlready = Colors.blue.shade900;
@@ -8,18 +6,6 @@ final Color linkColorDisabled = Colors.blue.shade200;
 const TextStyle hintStyle = TextStyle(color: Colors.black26);
 const TextStyle linkStyle = TextStyle(color: linkColor, decoration: TextDecoration.underline);
 
-Future<void> myLaunchUrl(String url, BuildContext context) async {
-  final Uri uri = Uri.parse(url);
-  if (await canLaunchUrl(uri)) {
-    try {
-      await launchUrl(uri);
-    } catch (e) {
-      await alertException(context, e);
-    }
-  } else {
-    await alert('Bad URL?', 'Could not launch "$uri"', ['okay'], context);
-  }
-}
 
 const kPadding = EdgeInsets.all(4);
 const kTallPadding = EdgeInsets.fromLTRB(0, 8, 0, 4);
